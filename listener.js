6496ae39ab25dd8bdbc744d8df22792504840a50
alpah121 <jacob_a_reed@yahoo.com>
2020-04-04 20:29:05 -0400

    created features for listener.js

---
+++ listener.js
@@ -0,0 +1,7 @@
+/*
+---FEATURES---
+-listens to marketplace events(loop of every 3 seconds)
+-holds onto a piece of a key mixed in with eleven random numbers
+-gets paid for giving out key piece
+-updates stats in stats file and daily log
+*/
